a96e31ba01f7b98589f7e7724dd5a4162d820c4c
leylaheydarova <lbadalzade@gmail.com>
2024-05-23 12:59:14 +0400

    Calculator app is done

---
+++ Calculator.App/Mathoperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.App
+{
+    public class Mathoperations
+    {
+        public double Sum(double num1, double num2)
+        {
+            double sum = num1 + num2;
+            return sum;
+        }
+
+        public double Multiplication(double num1, double num2)
+        {
+            double result = num1 * num2;
+            return result;
+        }
+
+        public double Division (double num1, double num2)
+        {
+            double result = 1;
+            if (num2 != 0)
+            {
+               result = num1 / num2;
+            }
+            else
+            {
+                Console.WriteLine("Can't divide by 0!");
+            }
+            return result;
+        }
+
+        public double Substraction (double num1, double num2) {
+            double remain = num1 - num2;
+            return remain;
+        }
+    }
+}
---
+++ Calculator.App/Program.cs
@@ -0,0 +1,59 @@
+﻿using Calculator.App;
+using System.Buffers;
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Task - Delegates:");
+
+bool isContinue = true;
+Mathoperations math = new Mathoperations();
+Operations operation = null;
+while (isContinue)
+{
+    Console.WriteLine("Calculator:\n1.Summation\n2.Multiplication\n3.Substraction\n4.Division\n0.Exit");
+    Console.WriteLine("Operation number:");
+    int.TryParse(Console.ReadLine(), out int optionnumber);
+    Console.Clear();
+    double res1 = new();
+    double res2 = new();
+
+    if (optionnumber != 0 && optionnumber <= 4)
+    {
+        (res1, res2) = InsertData();
+    }
+    switch (optionnumber)
+    {
+        case 0:
+            isContinue = false;
+            break;
+        case 1:            
+            operation = math.Sum;
+            break;
+        case 2:
+            operation = math.Multiplication;
+            break;
+        case 3:
+            operation = math.Substraction;
+            break;
+        case 4:
+            operation = math.Division;
+            break;
+        default:
+            Console.WriteLine("Invalid option number!");
+            break;
+    }
+    if(operation != null)
+    {
+        Console.WriteLine(operation(res1, res2));
+    }
+}
+
+(double, double) InsertData()
+{
+    Console.WriteLine("Number 1:");
+    double.TryParse(Console.ReadLine(), out double usernum1);
+    Console.WriteLine("Number 2:");
+    double.TryParse(Console.ReadLine(), out double usernum2);
+    return (usernum1, usernum2);    
+}
+
+public delegate double Operations(double num1, double num2);
